adce6428da85a2bc2252a220ab46d130de29c5d4
cristian4891 <cristianecossio@gmail.com>
2017-02-03 16:52:49 -0300

    Inconcluso Pinturas Ej 3 Tp

---
+++ jsFerretePinturas.js
@@ -0,0 +1,17 @@
+/*3.	Para el departamento de Pinturas:
+A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+*/
+function FahrenheitCentigrados () 
+{
+	var temperatura;
+	var FaCe;
+
+	temperatura=document.getElementById('temperatura').value;
+	FaCe=
+}
+
+function CentigradosFahrenheit () 
+{
+	
+}
