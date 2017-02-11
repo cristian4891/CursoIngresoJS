b028451a81b8268b3a03526be12f85c4aa2e6477
cristian4891 <cristianecossio@gmail.com>
2017-02-10 21:12:52 -0300

    Ej 1-6 Switch

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,36 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+case "Enero":
+case "Febrero":
+case "Marzo":
+case "Abril":
+case "Mayo":
+case "Junio":
+    alert("Falta para el invierno");
+break;
+
+case "julio":
+case "Agosto":
+    alert("Abrigate que hace frio");
+break;
+
+case "Septiembre":
+case "Octubre":
+case "Noviembre":
+case "Diciembre":
+    alert("Ya pasamos el frio, ahora calor!!!");
+break;
+
+
+}
+
+alert (mesDelAño);
+
+
+
+
+}//FIN DE LA FUNCIÓN
