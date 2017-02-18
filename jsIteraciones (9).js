38d660d59b94f4750b43e89d45287410f7e22538
cristian4891 <cristianecossio@gmail.com>
2017-02-17 22:09:51 -0300

    Ejercicio 9 practicar

--- jsIteraciones (9).js
+++ jsIteraciones (9).js
@@ -2,36 +2,151 @@ function Mostrar()
 {
 
 	var contador=0;
-	var numero;// declarar variables
+	var edad;
+	var sexo;
+	var nota;
+	var aprobado=0;
+	var nombre;
 	var maximo;
 	var minimo;
+	var nombredelmayor; 
+	var sexominimo;
+	var personasaprovadas;
+	var personasdesaprovadas;
+	var promedionotas;
+	var acumuladordenotas;
+	var contadormaculino;
+	var contadorfemenino;
+	var edadesmasculino;
+	var edadesfemenino;
+	var bandera="nadie";
+	var nombredeldiez;
+	var edaddeldiez;
+	var sexodeldiez;
+
+
+
+	               // declarar variables antes del while
+	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-	   contador++;
-	   numero=prompt("Ingrese un numero");
-	   numero=parseInt(numero);
-	   if (contador==1)
-	    {
-	    	maximo=numero;
-	    	minimo=numero;
-	    	
-	    }
-	   if (contador>1&&numero>maximo) 
-	    {
-            maximo=numero;
-	    }
-	   if (contador>1&&numero<minimo)
-	    {
-	    	minimo=numero;
-	    }  
-       respuesta=prompt("Ingrese si para continuar, o no para terminar");
-	
+
+         edad=prompt("Ingrese edad");
+         edad=parseInt(edad);
+            while(edad<0||edad>100)
+          {
+          		 edad=prompt("error,Ingrese edad");
+         		edad=parseInt(edad);
+          }
+
+         sexo=prompt("Por favor ingrese s o f segun sexo");
+             while(sexo!="f"&&sexo!="m")
+             {
+               sexo=prompt("Opcion incorrecta");
+             }
+         
+         nota=prompt("Ingrese una nota"); 
+         nota=parseInt(nota);
+             while(nota<0||nota>11)
+             {
+             	nota=prompt("Nota incorrecta");
+             } 
+
+         if (nota==10&&bandera=="nadie")
+          {
+          	 nombredeldiez=nombre;
+          	 edaddeldiez=edad;
+          	 sexodeldiez=sexo;
+          	 bandera="otro";
+          }
+         //cantidad de personas aprobadas
+         
+         if (nota>4)
+          {
+            aprobado=aprobado+1;
+          }  
+
+         //cuantas mujeres y cuantos hombres desaprobaron
+
+
+
+         //promedio de notas
+
+
+
+
+
+
+
+
+
+
+
+
+
+         //la edad promedio de las mujeres
+
+         if(sexo=="f")
+         {
+         	contadorfemenino++;
+         	edadesfemenino=edad+edadesfemenino;
+         }
+         else
+         {
+         	contadormasculino++;
+         	edadesmasculinos=edad+edadesmasculinos;
+         }
+         
+         //cantidad de mujeres que aprobaron la nota par y son menores de edad
+
+         if(sexo=="f"&&nota/2==0&&edad<18)
+           { if (nota>4) 
+
+                {
+                    contadormenorfemenino++; // mostrarlo al final por document.write cuando tengo que contar
+                }
+         //
+           }
+
+
+
+
+
+
+         contador++; // o contador+1
+         nombre=prompt("Por favor ingrese nombre");
+
+         if (contador==1)
+          {
+          	maximo=edad;
+          	minimo=edad;
+          	nombre=nombredelmayor;
+          }		
+         else
+          {
+          	if (edad>maximo)
+          	 {
+          	   	maximo=edad;
+          	   	nombredelmayor=nombre;
+          	 }
+          	if (edad<minimo)
+          	 {
+          	 	minimo=edad;
+          	 }
+
+
+
+          
+
+
+
+
+	 respuesta=prompt("Para salir ingrese no");
+
 	}
 
-    document.getElementById('maximo').value=maximo;
-    document.getElementById('minimo').value=minimo;
-
+    
 
 }//FIN DE LA FUNCIÓN
